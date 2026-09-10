bdbf0180b934e7e0e466d247e299e66b2b54015b
lourain <liyiweia@enn.cn>
2026-09-10 13:28:42 +0800

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { name } from './package.json'
 
 export default defineConfig({
+  base: `/${name}/`,
   plugins: [react(), tailwindcss()],
   server: { host: true, port: 5173 },
 })
